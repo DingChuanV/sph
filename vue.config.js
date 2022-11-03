a16eb41122f5d92ed0b5c9dd38ee3ac985d0d71f
wanglufei <2686221966@qq.com>
2022-11-03 17:40:29 +0800

    配置代理服务器

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,15 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 // 对外暴露的对象
 module.exports = defineConfig({
     //2.1 关闭eslint校验功能
     lintOnSave: false,
-    transpileDependencies: true
+    transpileDependencies: true,
+    //配置代理服务器
+    devServer: {
+        proxy: {
+            "/api": {
+                target: "http://gmall-h5-api.atguigu.cn"
+            }
+        }
+    }
 })
